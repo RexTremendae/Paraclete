2512f2d39e1bb3b551e2a45899978fc3e73646de
Per Westerlund <per@fastmail.se>
2023-05-20 23:21:15 +0200

    Frame and shortcut menu

--- MainLoop.cs
+++ MainLoop.cs
@@ -4,21 +4,22 @@ namespace Time;
 
 public static class MainLoop
 {
+    public static int updateInterval = 50;
+    private static int _windowHeight = 0;
+    private static int _windowWidth = 0;
+
     public static async Task Run()
     {
         var currentTimeSettings = new Settings() with {
-            UpdateInterval = 50,
             FontSize = 3,
-            DigitDisplayChar = '■',
+            Color = ConsoleColor.White,
             ShowHours = true,
             ShowSeconds = true,
             ShowMilliseconds = false
         };
 
         var stopWatchSettings = new Settings() with {
-            UpdateInterval = 50,
             FontSize = 2,
-            DigitDisplayChar = '■',
             ShowHours = true,
             ShowSeconds = true,
             ShowMilliseconds = true,
@@ -30,16 +31,29 @@ public static class MainLoop
         var currentTimeWriter = new TimeWriter(currentTimeSettings);
         var stopWatchWriter = new TimeWriter(stopWatchSettings);
 
-        var currentTimePosition = (x: CursorLeft, y: CursorTop);
-        var stopWatchPosition = (x: CursorLeft, y: CursorTop + 8);
+        var currentTimePosition = (x: 3, y: 2);
+        var stopWatchPosition = (x: 3, y: 12);
 
         ConsoleKey? key;
         var stopWatchStart = default(DateTime);
         var stopWatchStop = default(DateTime);
         var isStopWatchRunning = false;
 
+        var menuOptions = new[] {
+            (ConsoleKey.Escape, "Quit"),
+            (ConsoleKey.S, "Start/stop"),
+            (ConsoleKey.R, "Reset")
+        };
+
         for(;;)
         {
+            if (WindowWidth != _windowWidth || WindowHeight != _windowHeight)
+            {
+                _windowWidth = WindowWidth;
+                _windowHeight = WindowHeight;
+                PaintFrame(menuOptions);
+            }
+
             key = KeyAvailable ? ReadKey(true).Key : null;
 
             switch (key)
@@ -83,7 +97,51 @@ public static class MainLoop
                 - stopWatchStart;
             stopWatchWriter.Write(stopWatchTime, stopWatchPosition);
 
-            await Task.Delay(currentTimeSettings.UpdateInterval);
+            await Task.Delay(updateInterval);
+        }
+    }
+
+    private static void PaintFrame(params (ConsoleKey key, string description)[] menuOptions)
+    {
+        CursorLeft = 0;
+        CursorTop = 0;
+        var frameRows = new string[_windowHeight];
+        frameRows[0] = $"╔{"".PadLeft(_windowWidth-2, '═')}╗";
+        for (int y = 1; y < WindowHeight-1; y++)
+        {
+            if (y == 10 || y == WindowHeight-3)
+            {
+                frameRows[y] = $"╟{"".PadLeft(_windowWidth-2, '─')}╢";
+            }
+            else
+            {
+                frameRows[y] = $"║{"".PadLeft(_windowWidth-2)}║";
+            }
+        }
+        frameRows[_windowHeight-1] = $"╚{"".PadLeft(_windowWidth-2, '═')}╝";
+
+        for (int y = 0; y < _windowHeight; y++)
+        {
+            CursorLeft = 0;
+            CursorTop = y;
+            if (_windowHeight != WindowHeight || _windowWidth != WindowWidth)
+            {
+                return;
+            }
+            Write(frameRows[y]);
+        }
+
+        CursorLeft = 2;
+        CursorTop = _windowHeight-2;
+
+        foreach (var (key, description) in menuOptions)
+        {
+            Write("[");
+            ForegroundColor = ConsoleColor.Green;
+            Write(key);
+            ResetColor();
+            Write("] ");
+            Write(description + "    ");
         }
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -4,17 +4,15 @@ public readonly record struct Settings
 {
     public Settings()
     {
-        UpdateInterval = 50;
         Color = ConsoleColor.Gray;
         MillisecondsColor = ConsoleColor.DarkGray;
         ShowMilliseconds = false;
         ShowSeconds = true;
         ShowHours = true;
         FontSize = 2;
-        DigitDisplayChar = '#';
+        DigitDisplayChar = '█';
     }
 
-    public int UpdateInterval { get; init; }
     public ConsoleColor Color { get; init; }
     public ConsoleColor MillisecondsColor { get; init; }
     public bool ShowMilliseconds { get; init; }
