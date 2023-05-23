77fa3969e041444cf458843c830658b412974397
Per Westerlund <per@fastmail.se>
2023-05-23 10:15:52 +0200

    Clear screen and scroll history on startup

--- Program.cs
+++ Program.cs
@@ -2,8 +2,9 @@
 using Time;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
+// Similar to Clear() but also clears the scrollable buffer, which Clear() doesn't.
+Console.Write("\f\u001bc\x1b[3J");
 Console.CursorVisible = false;
-Console.Clear();
 
 var services = Configurator.Configure();
 
