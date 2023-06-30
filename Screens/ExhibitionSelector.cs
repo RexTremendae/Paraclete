c15aa60ff96da204e383276fc2f9d6c53acf7194
Per Westerlund <per@fastmail.se>
2023-06-30 23:40:44 +0200

    SA1306: Field 'Exhibitions' should begin with lower-case letter

--- Screens/ExhibitionSelector.cs
+++ Screens/ExhibitionSelector.cs
@@ -2,30 +2,30 @@ namespace Paraclete.Screens;
 
 public class ExhibitionSelector
 {
-    private readonly List<IExhibition> Exhibitions;
+    private readonly List<IExhibition> _exhibitions;
     private readonly ScreenInvalidator _screenInvalidator;
 
-    public int ExhibitionCount => Exhibitions.Count;
+    public int ExhibitionCount => _exhibitions.Count;
     public int SelectedExhibitionIndex { get; private set; }
-    public IExhibition SelectedExhibition => Exhibitions[SelectedExhibitionIndex];
+    public IExhibition SelectedExhibition => _exhibitions[SelectedExhibitionIndex];
 
     public void SelectNext()
     {
         SelectedExhibitionIndex++;
-        if (SelectedExhibitionIndex >= Exhibitions.Count) SelectedExhibitionIndex = 0;
+        if (SelectedExhibitionIndex >= _exhibitions.Count) SelectedExhibitionIndex = 0;
         _screenInvalidator.Invalidate();
     }
 
     public void SelectPrevious()
     {
         SelectedExhibitionIndex--;
-        if (SelectedExhibitionIndex < 0) SelectedExhibitionIndex = Exhibitions.Count-1;
+        if (SelectedExhibitionIndex < 0) SelectedExhibitionIndex = _exhibitions.Count-1;
         _screenInvalidator.Invalidate();
     }
 
     public ExhibitionSelector(IServiceProvider services, ScreenInvalidator screenInvalidator)
     {
-        Exhibitions = new (TypeUtility.EnumerateImplementatingInstancesOf<IExhibition>(services));
+        _exhibitions = new (TypeUtility.EnumerateImplementatingInstancesOf<IExhibition>(services));
         _screenInvalidator = screenInvalidator;
     }
 }
