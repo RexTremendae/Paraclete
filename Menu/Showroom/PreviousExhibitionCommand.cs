5cf41d058a0a1d6f748c079e20f5e63b5080bb9e
Per Westerlund <per@fastmail.se>
2023-06-30 23:09:01 +0200

    SA1013: Closing brace should be <preceded/followed> by a space

--- Menu/Showroom/PreviousExhibitionCommand.cs
+++ Menu/Showroom/PreviousExhibitionCommand.cs
@@ -19,4 +19,5 @@ public class PreviousExhibitionCommand : ICommand
     public PreviousExhibitionCommand(ExhibitionSelector exhibitionSelector)
     {
         _exhibitionSelector = exhibitionSelector;
-    }}
+    }
+}
