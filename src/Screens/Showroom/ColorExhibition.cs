cd8f7dd677fb6faa7901940b84878ba1a394dc21
Per Westerlund <per@fastmail.se>
2023-08-05 03:07:24 +0200

    Add background colors to color exhibition

--- src/Screens/Showroom/ColorExhibition.cs
+++ src/Screens/Showroom/ColorExhibition.cs
@@ -13,40 +13,75 @@ public class ColorExhibition : IExhibition
     public void Paint(Painter painter, (int x, int y) position, int paneIndex)
     {
         var (initialX, _) = position;
-
         var columnWidth = 16;
-        var colorNumber = 0;
+        var colorIdx = 0;
+
+        var rows = new List<AnsiStringBuilder>(Enumerable.Range(0, 5).Select(_ => new AnsiStringBuilder()));
 
         foreach (var color in Enum.GetValues<ConsoleColor>())
         {
-            var colorName = color switch {
-                    ConsoleColor.Black       => ForegroundColors.Black + BackgroundColors.Gray,
-                    ConsoleColor.DarkBlue    => ForegroundColors.DarkBlue,
-                    ConsoleColor.DarkGreen   => ForegroundColors.DarkGreen,
-                    ConsoleColor.DarkCyan    => ForegroundColors.DarkCyan,
-                    ConsoleColor.DarkRed     => ForegroundColors.DarkRed,
-                    ConsoleColor.DarkMagenta => ForegroundColors.DarkMagenta,
-                    ConsoleColor.DarkYellow  => ForegroundColors.DarkYellow,
-                    ConsoleColor.Gray        => ForegroundColors.Gray,
-                    ConsoleColor.DarkGray    => ForegroundColors.DarkGray,
-                    ConsoleColor.Blue        => ForegroundColors.Blue,
-                    ConsoleColor.Green       => ForegroundColors.Green,
-                    ConsoleColor.Cyan        => ForegroundColors.Cyan,
-                    ConsoleColor.Red         => ForegroundColors.Red,
-                    ConsoleColor.Magenta     => ForegroundColors.Magenta,
-                    ConsoleColor.Yellow      => ForegroundColors.Yellow,
-                    ConsoleColor.White       => ForegroundColors.White,
-                    _ => throw new NotImplementedException()
-                }
+            var fgColor = GetForegroundColorString(color);
+            var bgColor = AnsiSequences.ForegroundColors.Black + GetBackgroundColorString(color);
 
-            + color.ToString() + AnsiSequences.Reset;
+            var rowIdx = colorIdx < 8 ? 1 : 0;
+            rows[rowIdx].Append(fgColor.PadRight(columnWidth));
 
-            painter.Paint(colorName, position);
-            position = (colorNumber % 8 == 7)
-                ? (initialX, position.y + 1)
-                : (position.x + columnWidth, position.y);
+            rowIdx = colorIdx < 8 ? 4 : 3;
+            rows[rowIdx].Append(bgColor.PadRight(columnWidth));
 
-            colorNumber++;
+            colorIdx++;
         }
+
+        painter.PaintRows(rows.Select(_ => _.Build()), Layout.Panes[0], (1, 3));
+    }
+
+    private AnsiString GetForegroundColorString(ConsoleColor color)
+    {
+        return color switch {
+            ConsoleColor.Black       => ForegroundColors.Black + BackgroundColors.Gray,
+            ConsoleColor.DarkBlue    => ForegroundColors.DarkBlue,
+            ConsoleColor.DarkGreen   => ForegroundColors.DarkGreen,
+            ConsoleColor.DarkCyan    => ForegroundColors.DarkCyan,
+            ConsoleColor.DarkRed     => ForegroundColors.DarkRed,
+            ConsoleColor.DarkMagenta => ForegroundColors.DarkMagenta,
+            ConsoleColor.DarkYellow  => ForegroundColors.DarkYellow,
+            ConsoleColor.Gray        => ForegroundColors.Gray,
+            ConsoleColor.DarkGray    => ForegroundColors.DarkGray,
+            ConsoleColor.Blue        => ForegroundColors.Blue,
+            ConsoleColor.Green       => ForegroundColors.Green,
+            ConsoleColor.Cyan        => ForegroundColors.Cyan,
+            ConsoleColor.Red         => ForegroundColors.Red,
+            ConsoleColor.Magenta     => ForegroundColors.Magenta,
+            ConsoleColor.Yellow      => ForegroundColors.Yellow,
+            ConsoleColor.White       => ForegroundColors.White,
+            _ => throw new NotImplementedException()
+        }
+
+        + color.ToString() + AnsiSequences.Reset;
+    }
+
+    private AnsiString GetBackgroundColorString(ConsoleColor color)
+    {
+        return color switch {
+            ConsoleColor.Black       => BackgroundColors.Black + ForegroundColors.Gray,
+            ConsoleColor.DarkBlue    => BackgroundColors.DarkBlue,
+            ConsoleColor.DarkGreen   => BackgroundColors.DarkGreen,
+            ConsoleColor.DarkCyan    => BackgroundColors.DarkCyan,
+            ConsoleColor.DarkRed     => BackgroundColors.DarkRed,
+            ConsoleColor.DarkMagenta => BackgroundColors.DarkMagenta,
+            ConsoleColor.DarkYellow  => BackgroundColors.DarkYellow,
+            ConsoleColor.Gray        => BackgroundColors.Gray,
+            ConsoleColor.DarkGray    => BackgroundColors.DarkGray,
+            ConsoleColor.Blue        => BackgroundColors.Blue,
+            ConsoleColor.Green       => BackgroundColors.Green,
+            ConsoleColor.Cyan        => BackgroundColors.Cyan,
+            ConsoleColor.Red         => BackgroundColors.Red,
+            ConsoleColor.Magenta     => BackgroundColors.Magenta,
+            ConsoleColor.Yellow      => BackgroundColors.Yellow,
+            ConsoleColor.White       => BackgroundColors.White,
+            _ => throw new NotImplementedException()
+        }
+
+        + color.ToString() + AnsiSequences.Reset;
     }
 }
