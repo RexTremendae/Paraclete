8b1c7f0e7508a0ade570412e7e28e6bfd151b48d
Per Westerlund <per@fastmail.se>
2024-07-04 20:17:57 +0200

    Promote piece to queen (no choice dialog yet)

---
+++ src/Menu/Chess/States/PromotePieceState.cs
@@ -0,0 +1,25 @@
+﻿namespace Paraclete.Menu.Chess.States;
+
+using Paraclete.Menu.MenuStateHandling;
+using Paraclete.Modules.Chess;
+using Paraclete.Modules.Chess.PieceDefinitions;
+
+public class PromotePieceState(ChessBoard board, BoardSelectionService boardSelectionService)
+    : MenuStateBase<ChessMenuState, ChessMenuStateCommand>
+{
+    private readonly BoardSelectionService _boardSelectionService = boardSelectionService;
+    private readonly ChessBoard _board = board;
+
+    public override ChessMenuState State => ChessMenuState.PromotePiece;
+
+    public override IEnumerable<(ChessMenuStateCommand Command, bool PerformActions, ChessMenuState State)> Transitions =>
+    [
+        (ChessMenuStateCommand.Accept, true, ChessMenuState.SelectPiece),
+    ];
+
+    public override IMenuActionResult<ChessMenuState> EnterAction()
+    {
+        _board.PromotePiece(_boardSelectionService.ToMarkerPosition, new Queen());
+        return ForceTransition(ChessMenuState.SelectPiece, true);
+    }
+}
